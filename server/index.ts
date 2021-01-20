c218e8d7f5965bbea58c431b1fa64de28540ea2a
Alistair Vu <alistairvu@gmail.com>
2021-01-20 12:04:23 +0700

    Finish path change

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import { getRandomQuestion, addQuestion, addVote } from "./controller/methods"
 import { loadMain, loadAsk } from "./controller/render"
 const express = require("express")
+const path = require("path")
 
 const app = express()
 const port: number = 6960
@@ -8,6 +9,7 @@ const port: number = 6960
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "../public")))
 
 app.get("/", loadMain)
 app.get("/ask", loadAsk)
